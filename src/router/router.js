582a5e7ba0ebcdbc32c6e22b3db7979b67ce3b92
HusKey-dev <mypost31@mail.ru>
2022-04-08 20:05:13 +0300

    history to hash

--- src/router/router.js
+++ src/router/router.js
@@ -18,7 +18,7 @@ const routes = [
 
 const router = createRouter({
     routes,
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
 });
 
 export default router;
